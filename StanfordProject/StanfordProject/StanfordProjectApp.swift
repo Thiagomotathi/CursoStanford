eca41289e536713e7a434e568ef6c6cb53d25254
Thiago_mota _program <thiagomotamachado@gmail.com>
2024-05-22 09:27:45 -0300

    projeto iniciado

---
+++ StanfordProject/StanfordProject/StanfordProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordProjectApp.swift
+//  StanfordProject
+//
+//  Created by ThiagoMotaMachado on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct StanfordProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
