8b2e550f53088820ba56bf799a97b54728d326d7
Thiago_mota _program <thiagomotamachado@gmail.com>
2024-05-22 16:10:24 -0300

    develop memorize
    learn about @state

--- StanfordProject/StanfordProject/ContentView.swift
+++ StanfordProject/StanfordProject/ContentView.swift
@@ -7,17 +7,54 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
+struct ContentView: View {              //essa estrutura se comporta como uma view por isso existe a importação de um protocolo
+    var body: some View {               //views são como legos, segundo o cara de Stanford
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                CardView(isFaceUp: false)
+                CardView(isFaceUp: false)
+                CardView(isFaceUp: true)
+                CardView(isFaceUp: true)
+            }
+            .foregroundColor(.orange)
+            .padding()
         }
-        .padding()
     }
 }
+
+
+
+
+struct CardView: View {
+    @State var isFaceUp = false
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 20)
+            
+            if isFaceUp {
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 3)
+                Text("👻").font(.largeTitle)
+            }
+            else {
+                ZStack {
+                    base.foregroundColor(.orange)
+                }
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
